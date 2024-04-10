e09d228f4d4963930907693acc84dfbc0a65dbf0
TEParsons <todd.e.parsons@gmail.com>
2024-04-10 15:10:30 +0100

    doc: More info about example device stuff

--- psychopy_example_plugin/hardware/exampleDevice.py
+++ psychopy_example_plugin/hardware/exampleDevice.py
@@ -1,4 +1,9 @@
+from psychopy.hardware.base import BaseDevice
 
 
-class ExampleDevice:
-    pass
+class ExampleDevice(BaseDevice, aliases=["example"]):
+    """
+    Blank hardware object showcase how to make a new type of hardware object. If the hardware in question is a response device, such as a button box, you should check out the docs for [ExampleResponseDevice][psychopy_plugin.hardware.exampleResponseDevice.ExampleResponseDevice] instead.
+    
+    Should be deleted before publishing your plugin.
+    """
--- psychopy_example_plugin/hardware/exampleResponseDevice.py
+++ psychopy_example_plugin/hardware/exampleResponseDevice.py
@@ -1,4 +1,31 @@
+from psychopy.hardware.base import BaseResponseDevice, BaseResponse
 
 
-class ExampleResponseDevice:
-    pass
+class ExampleResponse(BaseResponse):
+    """ 
+    Blank hardware response object to showcase how to make a new type of hardware response for a ResponseDevice.
+
+    Parameters
+    ----------
+    t : float
+        Time at which the response happened
+    value
+        Value received (e.g. the key on a keyboard)
+    """
+
+class ExampleResponseDevice(BaseResponseDevice):
+    """
+    Blank hardware object to showcase how to make a new type of hardware object for a response device.
+    
+    Should be deleted before publishing your plugin.
+
+    Attributes
+    ----------
+    listeners : list[psychopy.hardware.listeners.BaseListener]
+        List of listeners to send responses to
+    responses : list[ExampleResponse]
+        List of responses received by this device object
+    muteOutsidePsychopy : bool
+        If True, then mute any responses gathered when the PsychoPy window is not in focus
+    """
+    responseClass = ExampleResponse
