70f388fcf7ea50d710c29b2a0d34508da905396d
Todd OST <todd.e.parsons@gmail.com>
2024-04-18 14:23:43 +0100

    DOC: Wrong ref in TestExampleComponent docs

--- tests/test_builder/test_ExampleComponent.py
+++ tests/test_builder/test_ExampleComponent.py
@@ -7,7 +7,7 @@ from psychopy.tests.test_experiment.test_components.test_base_components import 
 
 class TestExampleComponent(BaseComponentTests):
     """
-    Creating a subclass of _TestBaseComponentsMixin means that your testing class will inherit all of its methods - and will therefore run its tests on your Component. Just make sure to specify the Component class in the class attributes.
+    Creating a subclass of BaseComponentTests means that your testing class will inherit all of its methods - and will therefore run its tests on your Component. Just make sure to specify the Component class in the class attributes.
 
     Attributes
     ----------`
