bfe0529fad2a3dd1cf5ae92bb3dda82136e42bb7
TEParsons <todd.e.parsons@gmail.com>
2024-04-24 12:28:31 +0100

    ENH: Add example code writing for ExampleComponent

--- psychopy_example_plugin/components/exampleComponent/__init__.py
+++ psychopy_example_plugin/components/exampleComponent/__init__.py
@@ -1,4 +1,5 @@
-from psychopy.experiment.components import BaseComponent
+from psychopy.experiment.components import BaseComponent, Param, getInitVals
+from pathlib import Path
 
 class ExampleComponent(BaseComponent):
     """
@@ -7,4 +8,152 @@ class ExampleComponent(BaseComponent):
     # mark it as coming from this plugin
     plugin = "psychopy-example-plugin"
     # specify what libraries it has code for - PsychoPy and/or PsychoJS
-    targets = ["PsychoPy", "PsychoJS"]
+    targets = ["PsychoPy"]
+    # specify what category (or categories) to list this Component under in Builder
+    categories = ['Custom']
+    # path to this Component's icon file - ignoring the light/dark/classic folder and any @2x in the filename (PsychoPy will add these accordingly)
+    iconFile = Path(__file__).parent / "example.png"
+    # Text to display when this Component is hovered over
+    tooltip = "Example Component to show you how to use PsychoPy"
+    # what is the earliest version of PsychoPy this Component works with?
+    version = "0.0.0"
+    # is this Component still in beta?
+    beta = True
+
+    def __init__(
+        self, exp, parentName, 
+        # basic
+        name="",
+        # appearance
+        exampleAttribute="red",
+    ):
+        # initialise the base component class
+        BaseComponent.__init__(self, exp, parentName, name=name)
+        # base params like start and stop time are already added by BaseComponent, so add any other params in here...
+
+        # --- Params ---
+
+        # appearance
+        self.order += [
+            "exampleAttribute"
+        ]
+        self.params['exampleAttribute'] = Param(
+            exampleAttribute, valType="code", inputType="single", categ="Appearance",
+            label="Example attribute", hint="An example attribute to show you how to add one"
+        )
+    
+    def writeInitCode(self, buff):
+        """
+        Write the Python code which initialises the object for this Component.
+
+        Parameters
+        ----------
+        buff : 
+            String buffer to write to, i.e. the .py file
+        """
+        # any params using set each frame / repeat will need a safe value to start off with, this functon automatically substitutes those
+        inits = getInitVals(self.params)
+        # construct the actual code, using Python string formatting
+        # remember that params with valType="str" will already have quotes so you don't need to add them
+        code = (
+            "%(name)s = visual.ExampleVisualStim(\n"
+            "    win=win,\n"
+            "    name='%(name)s',\n"
+            ")\n"
+            "%(name)s.setExampleAttribute(%(exampleAttribute)s, log=False)\n"
+        )
+        # write the code to the string buffer (with params inserted)
+        buff.writeIndentedLines(code % inits)
+    
+    def writeInitCodeJS(self, buff):
+        """
+        This example only writes Python code, but if you want your Component to write JS you can use this function!
+
+        Parameters
+        ----------
+        buff : 
+            String buffer to write to, i.e. the .js file
+        """
+        pass
+
+    def writeRoutineStartCode(self, buff):
+        """
+        Write the Python code which is called at the start of this Component's Routine
+
+        Parameters
+        ----------
+        buff : 
+            String buffer to write to, i.e. the .py file
+        """
+        # update any parameters which need updating
+        self.writeParamUpdates(buff, updateType="set every repeat")
+        # aaaaand that's all you need! unless you want anything else to happen here - it's essentially the equivalent of the Begin Routine tab in a Code Component
+    
+    def writeFrameCode(self, buff):
+        """
+        Write the Python code which is called each frame for this Component.
+
+        Parameters
+        ----------
+        buff : 
+            String buffer to write to, i.e. the .py file
+        """
+        # update any parameters which need updating
+        self.writeParamUpdates(buff, updateType="set every frame")
+        # some code we want to run just once on the first frame of the Component - so we'll use the writeStartTestCode function to open an if statement and then dedent after
+        dedent = self.writeStartTestCode(buff)
+        # we only want the following code written if an if loop actually was opened, not if the start time is None! so make sure to use dedent as a boolean to avoid writing broken code
+        if dedent:
+            # status setting is already written by writeStartTestCode, so here we can just worry about extra stuff
+            code = (
+                "%(name)s.autoDraw = True\n"
+                "print('%(name)s has started! Yay!')\n"
+            )
+            buff.writeIndentedLines(code % self.params)
+            # dedent after!
+            buff.setIndentLevel(-dedent, relative=True)
+        
+        # use the same principle as we used for first-frame-of-Component code to add code which only runs while the Component is active
+        dedent = self.writeActiveTestCode(buff)
+        if dedent:
+            # here we can just add anything we want to happen each frame - let's update some arbitrary variable for fun
+            code = (
+                "%(name)s.someAttribute = randchoice(['a', 'b', 'c']))\n"
+            )
+            buff.writeIndentedLines(code % self.params)
+            # dedent after!
+            buff.setIndentLevel(-dedent, relative=True)
+        
+        # use the same principles again for last-frame-of-Component code
+        dedent = self.writeStopTestCode(buff)
+        if dedent:
+            # aaaaaaand some extra code for when the Component stops
+            code = (
+                "%(name)s.autoDraw = False\n"
+                "print('%(name)s has finished! Yay!')\n"
+            )
+            buff.writeIndentedLines(code % self.params)
+            # dedent after!
+            buff.setIndentLevel(-dedent, relative=True)
+    
+    def writeRoutineEndCode(self, buff):
+        """
+        Write the Python code which is called at the end of this Component's Routine
+
+        Parameters
+        ----------
+        buff : 
+            String buffer to write to, i.e. the .py file
+        """
+        # create a copy of params so that we can safely edit stuff
+        params = self.params.copy()
+        # add reference to the current loop (handy for data writing)
+        params['currentLoop'] = self.currentLoop
+        # store any data we'd like to store (start/stop are already handled)
+        code = (
+            "%(currentLoop)s.addData('%(name)s.someAttribute', %(name)s.someAttribute)"
+        )
+        buff.writeIndentedLines(code % params)
+
+        
+
--- psychopy_example_plugin/visual/exampleVisualStim.py
+++ psychopy_example_plugin/visual/exampleVisualStim.py
@@ -1,5 +1,5 @@
 from psychopy.visual.basevisual import BaseVisualStim
-
+from psychopy.tools.attributetools import attributeSetter, setAttribute
 
 class ExampleVisualStim(BaseVisualStim):
     """
@@ -21,3 +21,33 @@ class ExampleVisualStim(BaseVisualStim):
         Your stimulus class needs to define a `draw` function - otherwise you'll hit a NotImplementedError at the first win flip!
         """
         return
+    
+    @attributeSetter
+    def exampleAttribute(self, value):
+        """
+        Example attribute to show how to use the attributeSetter decorator.
+
+        Parameters
+        ----------
+        value : any
+            Whatever value you want to set
+        """
+        # attributeSetter handles all of the necessary logging stuff - so all you need to do is set the base value in this object's __dict__
+        self.__dict__['exampleAttribute'] = value
+        # you may want setting this attribute to do something else - let's say changing color if given one of three color names
+        if value in ("red", "green", "blue"):
+            self.fillColor = value
+    
+    def setExampleAttribute(self, value, log=True):
+        """
+        If an attribute can be set each frame/repeat from Builder, there needs to be a function called set<attribute name in PascalCase> as this will be called from the generated code.
+
+        Parameters
+        ----------
+        value : any
+            Whatever value you want to set
+        log : bool, optional
+            Whether or not to log setting this attribute - will be True for set each repeat and False for set each frame
+        """
+        setAttribute(self, "exampleAttribute", value=value, log=log)
+
