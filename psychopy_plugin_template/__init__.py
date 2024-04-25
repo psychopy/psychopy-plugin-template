6b3e2cdcdbff7d3d1717a96d1d51c586c5d8a3ee
Todd OST <todd.e.parsons@gmail.com>
2024-04-25 14:22:18 +0100

    RF: Rename module

---
+++ psychopy_plugin_template/__init__.py
@@ -0,0 +1,8 @@
+import importlib.metadata
+
+# to save yourself having to manually update the module version, you can use importlib to get it from the pyproject.toml!
+try:
+    __version__ = importlib.metadata.version("psychopy-plugin-template")
+except importlib.metadata.PackageNotFoundError:
+    # if you're running direct as a local module, you'll get a PackageNotFoundError, so in such instance let's just set the version to "dev"
+    __version__ = "dev"
