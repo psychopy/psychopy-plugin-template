8a1c1f6817a577ccf1dfce5c58b0798fe7f7583f
Todd OST <todd.e.parsons@gmail.com>
2024-04-25 14:30:59 +0100

    SYS: Use a valid dev number

--- psychopy_plugin_template/__init__.py
+++ psychopy_plugin_template/__init__.py
@@ -4,5 +4,5 @@ import importlib.metadata
 try:
     __version__ = importlib.metadata.version("psychopy-plugin-template")
 except importlib.metadata.PackageNotFoundError:
-    # if you're running direct as a local module, you'll get a PackageNotFoundError, so in such instance let's just set the version to "dev"
-    __version__ = "dev"
+    # if you're running direct as a local module, you'll get a PackageNotFoundError, so in such instance let's just set the version to "0.dev"
+    __version__ = "0.dev"
