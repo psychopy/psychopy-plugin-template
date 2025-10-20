93c36e9d4f8b96e8afac4ac3d1882a44cc8eb7a5
Todd OST <todd.e.parsons@gmail.com>
2025-10-20 15:19:36 +0100

    BF: Correct docs theme name

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -33,4 +33,4 @@ extensions = [
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'psychopy_plugin'
+html_theme = 'psychopy'
